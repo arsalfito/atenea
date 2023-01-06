a2b5ed747075991ff13b205abe4d5b43056b935c
alfonso <arsalfito@gmail.com>
2023-01-06 08:19:14 -0500

    Primer incremento

---
+++ servicios-activos-fijos/src/main/java/co/org/atenea/activos/ActivosFijosApplication.java
@@ -0,0 +1,17 @@
+package co.org.atenea.activos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class ActivosFijosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivosFijosApplication.class, args);
+	}
+
+}
